a8b2d3b728a7b81f5b67d7052eae80e49ed88b41
patriot <8457664+Patriot720@users.noreply.github.com>
2017-09-15 21:42:52 +0300

    First commit

---
+++ Follower.py
@@ -0,0 +1,170 @@
+from PyQt5.QtCore import QObject, pyqtSignal
+import os.path
+import time
+from urllib.error import (HTTPError)
+from instagram_web_api import Client, ClientCompatPatch, ClientError, ClientLoginError
+
+SLEEP_TIME = 60
+SLEEP_TIME_MODULE = 60
+STANDART_STOP_MESSAGE = "Остановлено"
+
+
+class Follower(QObject):
+    api_end = pyqtSignal(str)
+    end = pyqtSignal(str)
+    mid = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+        if(os.path.isfile("ignoreList.txt")):
+            ignore = open("ignoreList.txt", 'r')
+            self.ignore_list = ignore.read().splitlines()
+            ignore.close()
+        else:
+            self.ignore_list = []
+        self.functions = [
+            self.follow_subs_by_username,
+            self.follow_subs_by_hashtag,
+            self.follow_by_hashtag
+        ]
+        self._isRunning = True
+        self.following_ids = []
+
+    def follow(self):
+        try:
+            self.functions[self.index](self.text)
+        except ClientError as e:
+            print("ERR", e)
+            self.end.emit("Неверный ник/тег")
+        except ValueError:
+            self.end.emit("Неверный ник/тег")
+        except HTTPError:
+            self.end.emit("Слишком много запросов, придется подождать")
+        except:
+            self.end.emit("ОШИБКА ОШИБКА")
+
+    def start(self):
+        self._isRunning = True
+        self.get_api()
+        self.getFollowing()
+        self.follow()
+
+    def getFollowing(self):
+        has_next_page = True
+        end_cursor = None
+        while(has_next_page):
+            following = self.api.user_following(
+                self.id,extract=False,end_cursor=end_cursor,count=1000)
+            end_cursor = following['data']['user']['edge_follow']['page_info']['end_cursor']
+            has_next_page = following['data']['user']['edge_follow']['page_info']['has_next_page']
+            following_users = following['data']['user']['edge_follow']['edges']
+            for user in following_users:
+                self.following_ids.append(user['node']['id'])
+
+    def stop(self):
+        self._isRunning = False
+
+    def get_api(self):
+        try:
+            self.api = Client(
+                auto_patch=True, authenticate=True,
+                username=self.login, password=self.password)
+            self.id = self.api.authenticated_user_id
+            self.api_end.emit("Успешный вход в Инстаграмм")
+        except ClientLoginError:
+            self.end.emit("Неверный логин/пароль")
+            return
+        except (HTTPError, ClientError):
+            self.end.emit("Слишком много запросов, придется подождать")
+            return
+
+    def follow_subs_by_username(self, username):
+        max_id = None
+        has_next_page = 1
+        while(has_next_page):
+            user = self.api.user_info2(username)
+            followers = self.api.user_followers(
+                user['id'], max_id=max_id, count=1000)
+            if(not len(followers)):
+                return
+            max_id = followers[len(followers) - 1]['id']
+            has_next_page = len(followers)
+
+            for follower in followers:
+                if self.is_viable(follower):
+                    self.api.friendships_create(follower['id'])
+                    self.mid.emit(
+                        "Подписался на " + follower['username'] + "\nПолное имя: " + follower['full_name'])
+                    for i in range(SLEEP_TIME_MODULE):
+                        if(not self._isRunning):
+                            self.end.emit(STANDART_STOP_MESSAGE)
+                            return
+                        time.sleep(SLEEP_TIME / SLEEP_TIME_MODULE)
+        self.end.emit('Подписка закончена')  # TAA SHAA
+
+    def follow_subs_by_hashtag(self, hashtag):
+        max_id = None
+        has_next_page = True
+        while(has_next_page):
+            res = self.api.tag_feed(hashtag, max_id=max_id)
+            media = res['tag']['media']
+            has_next_page = media['page_info']['has_next_page']
+            max_id = media['page_info']['end_cursor']
+            items = media['nodes']
+            for item in items:
+                users_max_id = 0
+                users_has_next_page = 1
+                user_id = item['owner']['id']
+                while(users_has_next_page):
+                    followers = self.api.user_followers(
+                        user_id, max_id=users_max_id, count=1000)
+                    for follower in followers:
+                        if self.is_viable(follower):
+                            self.api.friendships_create(follower['id'])
+                            self.mid.emit(
+                                "Подписался на " + follower['username'] + "Полное имя: " + follower['full_name'])
+                            for i in range(SLEEP_TIME_MODULE):
+                                if(not self._isRunning):
+                                    self.end.emit(STANDART_STOP_MESSAGE)
+                                    return
+                                time.sleep(SLEEP_TIME / SLEEP_TIME_MODULE)
+        self.end.emit('Подписка закончена')
+
+    def is_viable(self, follower):
+        return (follower['id'] not in self.ignore_list and follower['id'] not in self.following_ids)
+
+    def follow_by_hashtag(self, hashtag):
+        max_id = None
+        has_next_page = True
+        while(has_next_page):
+            res = self.api.tag_feed(hashtag, max_id=max_id)
+            media = res['tag']['media']
+            has_next_page = media['page_info']['has_next_page']
+            max_id = media['page_info']['end_cursor']
+            items = media['nodes']
+            for item in items:
+                follower = item['owner']['id']
+                self.api.friendships_create(follower)
+                self.mid.emit(
+                    "Подписался на " + follower)
+                for i in range(SLEEP_TIME_MODULE):
+                    if(not self._isRunning):
+                        self.end.emit(STANDART_STOP_MESSAGE)
+                        return
+                    time.sleep(SLEEP_TIME / SLEEP_TIME_MODULE)
+        self.end.emit("Подписка закончена")
+
+
+if __name__ == '__main__':
+    print('kappa')
+    em = Follower()
+    em.login = "alarmavape@yandex.ru"
+    em.password = "alarmattention"
+    em.get_api()
+    em.getFollowing()
+    follower = {
+        'id': '2886391938'
+    }
+    print(follower['id'])
+    viable = em.is_viable(follower)
+    print('es')
---
+++ UI.py
@@ -0,0 +1,138 @@
+# -*- coding: utf8 -*-
+from PyQt5.QtCore import QThread
+from PyQt5 import uic
+from PyQt5.QtWidgets import (QDialog, QPushButton, QStatusBar, QLabel, QLineEdit,
+                             QTextEdit, QApplication, QMainWindow, QMessageBox)
+import os.path
+import time
+import sys
+from Follower import Follower
+
+
+class UI(QMainWindow):
+    label_types = [
+        "Ник",
+        "Хештег без #",
+        "Хештег без #"
+    ]
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.follower = Follower()
+        self.thread = QThread()
+        self.follower.moveToThread(self.thread)
+        self.thread.started.connect(self.follower.start)
+        self.follower.api_end.connect(self.on_api_end)
+        self.follower.end.connect(self.on_end)
+        self.follower.mid.connect(self.on_mid)
+        self.type_chooser.currentIndexChanged.connect(self.on_index_change)
+        self.start_btn.clicked.connect(self.on_start)
+        lp = self.getLPFromCache()
+        if(lp):
+            self.login.setText(lp[0])
+            self.password.setText(lp[1])
+        self.show()
+
+    def getLPFromCache(self):
+        vm = []
+        if(os.path.isfile("login.txt")):
+            f = open("login.txt", "r")
+            vm = f.read().splitlines()
+            f.close()
+            if(len(vm) != 2):
+                vm = []
+        return vm
+
+    def on_index_change(self, index):
+        self.type_label.setText(self.label_types[index])
+
+    def on_api_end(self, msg):
+        self.progress_box.append(msg)
+
+    def launch(self):
+        self.follower.login = self.login.text()
+        self.follower.password = self.password.text()
+        self.follower.index = self.type_chooser.currentIndex()
+        self.follower.text = self.choose_box.text()
+        self.thread.start()
+
+    def on_end(self, msg):
+        self.progress_box.append(msg)
+        self.enableBtn()
+        self.start_btn.setChecked(False)
+        self.stop_thread()
+
+    def enableBtn(self):
+        self.start_btn.setText("Старт")
+
+    def disableBtn(self):
+        self.start_btn.setText("Стоп")
+
+    def on_mid(self, msg):
+        self.progress_box.append(msg)
+
+    def saveLP(self):
+        login = self.login.text()
+        password = self.password.text()
+        f = open("login.txt", 'w')
+        f.write(login + "\n" + password)
+        f.close()
+
+    def stop_thread(self):
+        self.follower.stop()
+        self.thread.quit()
+        self.thread.wait()
+
+    def on_start(self, checked):
+        if(checked):
+            self.saveLP()
+            self.launch()
+            self.disableBtn()
+        else:
+            self.enableBtn()
+            self.stop_thread()
+
+
+def excepthook(excType, excValue, tracebackobj):
+    """
+    Global function to catch unhandled exceptions.
+
+    @param excType exception type
+    @param excValue exception value
+    @param tracebackobj traceback object
+    """
+    separator = '-' * 80
+    logFile = "simple.log"
+    notice = \
+        """An unhandled exception occurred. Please report the problem\n"""\
+        """using the error reporting dialog or via email to <%s>.\n"""\
+        """A log has been written to "%s".\n\nError information:\n""" % \
+        ("yourmail at server.com", "")
+    versionInfo = "0.0.1"
+    timeString = time.strftime("%Y-%m-%d, %H:%M:%S")
+
+    tbinfofile = cStringIO.StringIO()
+    traceback.print_tb(tracebackobj, None, tbinfofile)
+    tbinfofile.seek(0)
+    tbinfo = tbinfofile.read()
+    errmsg = '%s: \n%s' % (str(excType), str(excValue))
+    sections = [separator, timeString, separator, errmsg, separator, tbinfo]
+    msg = '\n'.join(sections)
+    try:
+        f = open(logFile, "w")
+        f.write(msg)
+        f.write(versionInfo)
+        f.close()
+    except IOError:
+        pass
+    errorbox = QMessageBox()
+    errorbox.setText(str(notice) + str(msg) + str(versionInfo))
+    errorbox.exec_()
+
+
+sys.excepthook = excepthook
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = UI()
+    sys.exit(app.exec_())
